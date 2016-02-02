2d02998416a8598c29092aebb0ac8c8031454177
ppoteralski <poteralski.dev@gmail.com>
2016-02-02 21:24:09 +0100

    helper class

---
+++ src/pl/poteralski/stackoverflow/StackExchangeUri.java
@@ -0,0 +1,21 @@
+package pl.poteralski.stackoverflow;
+
+/**
+ *
+ * Created by codeninja on 02.02.16.
+ */
+public class StackExchangeUri {
+    public static String API_URL = "http://api.stackexchange.com/2.2/search/advanced";
+    public static String ORDERING = "order";
+    public static String ORDERING_DESC = "desc";
+    public static String ORDERING_ASC = "asc";
+    public static String SORT = "sort";
+    public static String SORT_ACTIVITY = "activity";
+    public static String ACCEPTED = "activity";
+    public static String ACCEPTED_TRUE = "True";
+    public static String ACCEPTED_FALSE = "False";
+    public static String TAGGED = "tagged";
+    public static String TITLE = "title";
+    public static String SITE = "site";
+    public static String SITE_SO = "stackoverflow";
+}
--- src/pl/poteralski/stackoverflow/StackOverflowHelper.java
+++ src/pl/poteralski/stackoverflow/StackOverflowHelper.java
@@ -5,20 +5,48 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * Created by codeninja on 01.02.16.
  */
 public class StackOverflowHelper {
+
     public String getSnippet(String selectedText) throws UnirestException {
-        HttpResponse<JsonNode> jsonArray = Unirest.get("https://api.stackexchange.com/2.2/search/advanced")
-                .queryString("order", "desc")
-                .queryString("sort", "activity")
-                .queryString("accepted", "True")
-                .queryString("tagged", "python%3Blxml")
-                .queryString("title", "parse%20html")
-                .queryString("site", "stackoverflow")
-                .asJson();
+        String title = removeHashTagsFromText(selectedText);
+        String preparedHashTags = prepareHashTags(getHashTags(selectedText));
+        HttpResponse<JsonNode> jsonArray = makeHttpRequest(title, preparedHashTags);
         return jsonArray.getBody().getArray().toString();
+    }
+
+    private HttpResponse<JsonNode> makeHttpRequest(String title, String preparedTags) throws UnirestException {
+        return Unirest.get(StackExchangeUri.API_URL)
+            .queryString(StackExchangeUri.SITE, StackExchangeUri.SITE_SO)
+            .queryString(StackExchangeUri.ORDERING, StackExchangeUri.ORDERING_DESC)
+            .queryString(StackExchangeUri.SORT, StackExchangeUri.SORT_ACTIVITY)
+            .queryString(StackExchangeUri.ACCEPTED, StackExchangeUri.ACCEPTED_TRUE)
+            .queryString(StackExchangeUri.TAGGED, preparedTags)
+            .queryString(StackExchangeUri.TITLE, title)
+            .asJson();
+    }
+
+    private List<String> getHashTags(String text){
+        Pattern pattern = Pattern.compile("#(\\w+|\\W+)");
+        Matcher mat = pattern.matcher(text);
+        List<String> tags = new ArrayList<>();
+        while (mat.find()) tags.add(mat.group(1));
+        return tags;
+    }
+
+    private String prepareHashTags(List<String> hashTagsList){
+        return String.join(";", hashTagsList);
+    }
+
+    private String removeHashTagsFromText(String text){
+        return text.replaceAll("#(\\w+|\\W+)", "");
     }
 }
